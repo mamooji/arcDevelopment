3ec4850ab95a02bd71660340d028ae9ea7835343
mamooji <mamoojim@hotmail.com>
2020-10-08 12:12:00 -0400

    almost finished with the landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -9,7 +9,11 @@ import Typography from "@material-ui/core/Typography";
 import customSoftwareIcon from "../assets/Custom Software Icon.svg";
 import mobileAppsIcon from "../assets/mobileIcon.svg";
 import websitesIcon from "../assets/websiteIcon.svg";
+import repeatingBkg from "../assets/repeatingBackground.svg";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import informationBkg from "../assets/infoBackground.svg";
 const useStyles = makeStyles((theme) => ({
   animation: {
     maxWidth: "50em",
@@ -84,15 +88,47 @@ const useStyles = makeStyles((theme) => ({
       padding: 25,
     },
   },
+  revolutionBkg: {
+    backgroundImage: `url(${repeatingBkg})`,
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    height: "100%",
+    width: "100%",
+  },
+  infoBkg: {
+    backgroundImage: `url(${informationBkg})`,
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    height: "100%",
+    width: "100%",
+  },
+  revolutionCard: {
+    position: "absolute",
+    boxShadow: theme.shadows[10],
+    borderRadius: 15,
+    padding: "10em",
+    [theme.breakpoints.down("sm")]: {
+      paddingTop: "8em",
+      paddingBotton: "8em",
+      paddingRight: 0,
+      paddingLeft: 0,
+      borderRadius: 0,
+      width: "100%",
+    },
+  },
 }));
 const LandingPage = (props) => {
   const classes = useStyles();
   const theme = useTheme();
+
   const matchesSM = useMediaQuery(theme.breakpoints.down("sm"));
+  const matchesXS = useMediaQuery(theme.breakpoints.down("xs"));
 
   const defaultOptions = {
     loop: true,
-    autoplay: true,
+    autoplay: false,
     animationData: animationData,
     rendererSettings: {
       preserveAspectRatio: "xMidYMid slice",
@@ -266,6 +302,124 @@ const LandingPage = (props) => {
               src={websitesIcon}
             />
           </Grid>
+        </Grid>
+      </Grid>
+
+      {/*--------{Revolution Block}------- */}
+      <Grid item>
+        <Grid
+          container
+          alignItems="center"
+          justify="center"
+          style={{ height: "100em", marginTop: "12em" }}
+        >
+          <Card className={classes.revolutionCard}>
+            <CardContent>
+              <Grid
+                container
+                direction="column"
+                style={{ textAlign: "center" }}
+              >
+                <Grid item>
+                  <Typography variant="h3" gutterBottom>
+                    The Revolution
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Typography variant="subtitle1">
+                    Visionary insights coupled with cutting-edge technology is a
+                    recipe for revolution
+                  </Typography>
+                  <Button variant="outlined" className={classes.learnButton}>
+                    <span style={{ marginRight: 10 }}>Learn More</span>
+                    <ButtonArrow
+                      width={10}
+                      height={10}
+                      fill={theme.palette.common.blue}
+                    />
+                  </Button>
+                </Grid>
+              </Grid>
+            </CardContent>
+          </Card>
+          <div className={classes.revolutionBkg} />
+        </Grid>
+      </Grid>
+      <Grid item>
+        {/*--------{AboutUS Block}------- */}
+        <Grid
+          container
+          alignItems="center"
+          style={{ height: "80em" }}
+          direction="row"
+        >
+          <Grid
+            item
+            container
+            style={{
+              position: "absolute",
+              textAlign: matchesXS ? "center" : "inherit",
+            }}
+            direction={matchesXS ? "column" : "row"}
+            spacing={matchesXS ? 10 : 0}
+          >
+            <Grid
+              item
+              sm
+              style={{
+                marginLeft: matchesXS ? 0 : matchesSM ? "2em" : "5em",
+              }}
+            >
+              <Grid container direction="column">
+                <Typography variant="h2" style={{ color: "white" }}>
+                  About Us
+                </Typography>
+                <Typography variant="subtitle2">Lets get personal</Typography>
+                <Grid item>
+                  <Button
+                    variant="outlined"
+                    style={{ color: "white", borderColor: "white" }}
+                    className={classes.learnButton}
+                  >
+                    <span style={{ marginRight: 10 }}>Learn More</span>
+                    <ButtonArrow width={10} height={10} fill="white" />
+                  </Button>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid
+              item
+              sm
+              style={{
+                textAlign: matchesXS ? "center" : "right",
+                marginRight: matchesXS ? 0 : matchesSM ? "2em" : "5em",
+              }}
+            >
+              <Grid container direction="column">
+                <Typography variant="h2" style={{ color: "white" }}>
+                  Contact Us
+                </Typography>
+                <Typography variant="subtitle2">
+                  Say hello!
+                  <span role="img" aria-label="waving hand">
+                    👋
+                  </span>
+                </Typography>
+                <Grid item>
+                  <Button
+                    variant="outlined"
+                    style={{ color: "white", borderColor: "white" }}
+                    className={classes.learnButton}
+                  >
+                    <span style={{ marginRight: 10 }}>Learn More</span>
+                    <ButtonArrow width={10} height={10} fill="white" />
+                  </Button>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+
+          <div className={classes.infoBkg} />
         </Grid>
       </Grid>
     </Grid>
